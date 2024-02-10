71a76bf033c346891a6d83fd1817a260905a7dd5
Melanie Winter <melaniebucher830@gmail.com>
2024-02-10 01:39:48 -0800

    Fixes mobile clicking issue

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('hello world!')
+const projectCards = document.querySelectorAll('.project-card');
+const projectCardOverlays = document.querySelectorAll('.overlay');
+
+projectCards.forEach((projectCard, index) => {
+    projectCard.addEventListener('click', () => {
+        projectCardOverlays[index].style.display = 'flex';
+        console.log('clicked');
+    });
+});
+
+// const projectCards = document.querySelectorAll('.project-card');
+// console.log(projectCards); // Check if projectCards contains elements
+
+// projectCards.forEach((projectCard, index) => {
+//     projectCard.addEventListener('click', () => {
+//         console.log('clicked'); // Check if click event is triggered
+//     });
+// });
