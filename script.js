9dc794cff51bff1f0e5cdfe1ac922bcfe8e0775e
Melanie Winter <melaniebucher830@gmail.com>
2024-02-13 20:57:27 -0800

    Adds new resume

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 const projectCards = document.querySelectorAll('.project-card');
 const projectCardOverlays = document.querySelectorAll('.overlay');
 
-console.log('PROJECT CARDS', projectCards)
-console.log('PROJECT CARD OVERLAYS', projectCardOverlays)
-
 projectCards.forEach((projectCard, index) => {
-    console.log('PRC INDEX', projectCard[index])
     projectCard.addEventListener('click', () => {
         projectCardOverlays[index].style.display = 'flex';
-        console.log('PCO INDEX', projectCardOverlays[index])
-        console.log('clicked');
     });
 });
